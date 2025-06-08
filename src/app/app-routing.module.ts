13ea84df13cfb589d2dbd0777556b7532e719898
HP <vdibbamaduguv57@gmail.com>
2025-06-08 19:18:28 +0530

    inital commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './Authentication/sign-up/sign-up.component';
+import { LoginComponent } from './Authentication/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
